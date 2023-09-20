4883d588fddbb6403b397187966fe52ec0cf89ff
Ahmed <Ahmedparkar21303@gmail.com>
2023-09-20 21:47:55 +0530

    data collection
    added virtual environment and data collection api

---
+++ Flask_API/api.py
@@ -0,0 +1,141 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+from typing import Dict
+import jmespath
+from parsel import Selector
+from nested_lookup import nested_lookup
+from playwright.sync_api import sync_playwright
+
+import googleapiclient.discovery
+import googleapiclient.errors
+
+api_service_name = "youtube"
+api_version = "v3"
+DEVELOPER_KEY = "AIzaSyCpxaf76fvm5jEHe1IKh1Wh8ZEOD-FdzWQ"
+
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+
+
+def parse_thread(data: Dict) -> Dict:
+    """Parse Twitter tweet JSON dataset for the most important fields"""
+    result = jmespath.search(
+        """{
+        text: post.caption.text,
+        published_on: post.taken_at,
+        id: post.id,
+        pk: post.pk,
+        code: post.code,
+        username: post.user.username,
+        user_pic: post.user.profile_pic_url,
+        user_verified: post.user.is_verified,
+        user_pk: post.user.pk,
+        user_id: post.user.id,
+        has_audio: post.has_audio,
+        reply_count: view_replies_cta_string,
+        like_count: post.like_count,
+        images: post.carousel_media[].image_versions2.candidates[1].url,
+        image_count: post.carousel_media_count,
+        videos: post.video_versions[].url
+    }""",
+        data,
+    )
+    result["videos"] = list(set(result["videos"] or []))
+    if result["reply_count"]:
+        result["reply_count"] = int(result["reply_count"].split(" ")[0])
+    result[
+        "url"
+    ] = f"https://www.threads.net/@{result['username']}/post/{result['code']}"
+    return result
+
+
+def scrape_thread(url: str) -> dict:
+    """Scrape Threads post and replies from a given URL"""
+    with sync_playwright() as pw:
+        # start Playwright browser
+        browser = pw.chromium.launch()
+        context = browser.new_context(viewport={"width": 1920, "height": 1080})
+        page = context.new_page()
+
+        # go to url and wait for the page to load
+        page.goto(url)
+        # wait for page to finish loading
+        page.wait_for_selector("[data-pressable-container=true]")
+        # find all hidden datasets
+        selector = Selector(page.content())
+        hidden_datasets = selector.css('script[type="application/json"][data-sjs]::text').getall()
+        # find datasets that contain threads data
+        for hidden_dataset in hidden_datasets:
+            # skip loading datasets that clearly don't contain threads data
+            if '"ScheduledServerJS"' not in hidden_dataset:
+                continue
+            if "thread_items" not in hidden_dataset:
+                continue
+            data = json.loads(hidden_dataset)
+            # datasets are heavily nested, use nested_lookup to find
+            # the thread_items key for thread data
+            thread_items = nested_lookup("thread_items", data)
+            if not thread_items:
+                continue
+            # use our jmespath parser to reduce the dataset to the most important fields
+            threads = [parse_thread(t) for thread in thread_items for t in thread]
+            return {
+                # the first parsed thread is the main post:
+                "thread": threads[0],
+                # other threads are replies:
+                "replies": threads[1:],
+            }
+        raise ValueError("could not find thread data in page")
+
+
+def get_threads_replies(link):
+    scraped = scrape_thread(link)
+    replies = []
+    for i in scraped["replies"]:
+        replies.append((i["text"],i['like_count']))
+    return replies
+
+def get_youtube_comments(link):
+    v_id = link[-11:]
+    request = youtube.commentThreads().list(
+    part="snippet",
+    videoId=v_id,
+    maxResults=100)
+    response = request.execute()
+
+    comments = []
+
+    for item in response['items']:
+        comment = item['snippet']['topLevelComment']['snippet']
+        comments.append((
+            comment['textDisplay'],
+            comment['likeCount'])
+
+        )
+
+
+    return comments
+
+
+def get_yelp_reviews(link):
+    r = requests.get(link)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    regex = re.compile('.*comment.*')
+    results = soup.find_all('p', {'class':regex})
+    usefulness = soup.find_all('span',class_ = 'css-1lr1m88')
+    reviews= []
+    for i in range(len(usefulness)):
+        try:
+            reviews.append((results[i].text,int(usefulness[::3][i].text[1:])))
+        except:
+            pass
+    return reviews
+
+
+# print(get_threads_replies("https://www.threads.net/@zuck/post/CxWMc87galo"))
+print(get_threads_replies("https://www.threads.net/@zuck/post/CxMVWgUrNWY"))
+
+
