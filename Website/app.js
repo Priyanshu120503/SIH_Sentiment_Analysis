72456d4a1fbebf8486950fbac3ee54c321b42265
Jinesh3578 <jineshshah2512@gmail.com>
2023-09-21 12:52:35 +0530

    Updated app.js

--- Website/app.js
+++ Website/app.js
@@ -1,64 +1,64 @@
-//jshint esversion:6
+// jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const https = require("https");
 
 const app = express();
 
-
-<<<<<<< HEAD
 app.set("view engine", "ejs");
-app.set("views", __dirname + "\\views")
-=======
-app.set('view engine', 'ejs');
-
->>>>>>> 592d393f79197f245f9600cf9ae1ef536539f373
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("views", __dirname + "\\views");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "\\public"));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/view", function(req, res){
+app.get("/view", function (req, res) {
   res.render("view");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function (req, res) {
   res.render("register");
 });
 
-
-app.get("/login", function(req, res){
-  res.render("login")
+app.get("/login", function (req, res) {
+  res.render("login");
 });
 
-app.post("/data", function(req, res){
+// Make the route handler async
+app.post("/data", async function (req, res) {
   const object = {
-    level : req.body.site,
-    link : req.body.link
+    level: req.body.site,
+    link: req.body.link,
+  };
+
+  try {
+    // Use 'await' inside the 'async' function
+    const response = await fetch("http://127.0.0.1:5000/data/" + encodeURIComponent(object.link), {
+      method: "GET",
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+
+      res.render("view", {
+        variable: object,
+        data: result,
+      });
+    } else {
+      // Handle the case where the response is not OK
+      res.status(500).send("Error fetching data from the server");
+    }
+  } catch (error) {
+    // Handle any errors that occur during the fetch or rendering
+    console.error(error);
+    res.status(500).send("Error fetching data from the server");
   }
-
-  // console.log(object);
-  // console.log("http://127.0.0.1:5000/data/" + encodeURIComponent(object.link));
-
-  result = await fetch("http://127.0.0.1:5000/data/" + encodeURIComponent(object.link), {
-    method: "GET"
-  }).then(res => res.json()).then(data => data);
-  
-  // console.log(result);
-  
-  res.render("view",{
-    variable : object,
-    data: result
-  })
 });
 
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000 boi");
 });
-
